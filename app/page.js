c376e92b68a3e53b23ce9db8a3d6da84a1928d78
hhe107-SU <143746082+hhe107-SU@users.noreply.github.com>
2024-04-15 02:09:37 -0400

    attendance: videos 04/04/24 fetch data from the url

--- app/page.js
+++ app/page.js
@@ -1,26 +1,65 @@
 "use client";
 
+import {useState, useEffect} from "react";
+import Image from 'next/image';
+
 
 
 import ButtonDemo from "../components/ButtonDemo"
 import Button from "../components/Button";
-import {getPeople} from "../lib/api";
+
 
 import ColorPicker from "../components/ColorPicker";
-import PeoplePicker from "../components/PeoplePicker"
+import PeoplePicker from "../components/PeoplePicker";
+import {getPeople, getWeatherData} from "../lib/api";
 
 
 const Homepage =()=>{
+  const [weatherData,setWeatherData]=useState(null);
+
   const peopleArr = getPeople();
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getWeatherData();
+      setWeatherData(response);
+
+    };
+    fetchData();
+
+
+  },[]);
+
+
   //console.log({peopleArr})
 
   return (
   <div>
+  
   <h1>wheather app</h1>
-  <PeoplePicker people={peopleArr} />
+
+  {weatherData &&(<div>
+    <h2>{weatherData.city.name}</h2>
+    <p>Current temp:{weatherData.list[0].main.temp}&deg; F</p>
+    <p>{weatherData.list[0].weather[0].description}</p>
+    <Image 
+          src={`https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}.png`}
+          alt={`Weather icon`}
+          width ={100}
+          height={100}
+
+        />
+  </div>
+  
+  
+  )}
+
+
+  {/*<PeoplePicker people={peopleArr} />
   <ButtonDemo />
  
-  <ColorPicker/>
+  <ColorPicker/>*/}
   </div>
   );
 
